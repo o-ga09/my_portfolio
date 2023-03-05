cb5cca40258e1542dea27973aab2b667152a9a1b
taiti09 <taitiabe1997@gmail.com>
2023-03-05 13:13:20 +0900

    refact:不要ファイル削除

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,10 @@
     <header class="text-gray-600 body-font">
       <div class="header-container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a class="mr-5 hover:text-gray-900">Home</a>
-          <a class="mr-5 hover:text-gray-900">Profile</a>
-          <a class="mr-5 hover:text-gray-900">Skills</a>
-          <a class="mr-5 hover:text-gray-900">Hobby</a>
+          <a href="/" class="mr-5 hover:text-gray-900">Home</a>
+          <a href="https://sites.google.com/view/profile202303/%E3%83%9B%E3%83%BC%E3%83%A0" class="mr-5 hover:text-gray-900">Profile</a>
+          <a href="https://sites.google.com/view/skills-202303/%E3%83%9B%E3%83%BC%E3%83%A0" class="mr-5 hover:text-gray-900">Skills</a>
+          <a href="https://sites.google.com/view/vlogs202303/%E3%83%9B%E3%83%BC%E3%83%A0" class="mr-5 hover:text-gray-900">Blog</a>
         </nav>
       </div>
     </header>
