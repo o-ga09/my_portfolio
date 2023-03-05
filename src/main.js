a47c20e5d4b1fd4c30f56a9f54df0b5a73badc48
taiti09 <taitiabe1997@gmail.com>
2023-03-05 16:40:12 +0900

    revert:ふよファイル削除

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import router from './routes'
 import axios from 'axios'
 import './index.css'  //tailwind 用に追記
 import '@mdi/font/css/materialdesignicons.css' //material UI 用
+import VueMeta from 'vue-meta'
 
 Vue.config.productionTip = false
-
+Vue.use(VueMeta)
 new Vue({
   axios,
   router,
