855d67c09c644f67911d74d4c4d897efef1faf19
o-ga <54522966+o-ga09@users.noreply.github.com>
2023-04-17 15:16:09 +0900

    feat: #5 追加ページルーティング変更

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './pages/self-introduction.vue'
+import Skill from './pages/Skill.vue'
+import Portfolio from './pages/Portfolio.vue'
+import Blog from './pages/Blog.vue'
 
 Vue.use(Router)
 
@@ -8,9 +12,24 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/Home',
       name: 'self-introduction',
-      component: () => import('./pages/self-introduction.vue')
+      component: Home
+    },
+    {
+      path: '/Skills',
+      name: 'self-introduction',
+      component: Skill
+    },
+    {
+      path: '/Portfolio',
+      name: 'self-introduction',
+      component: Portfolio
+    },
+        {
+      path: '/Blog',
+      name: 'self-introduction',
+      component: () => Blog
     }
   ]
 })
