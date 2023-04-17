945c46c3ff7bb57ccf4c5e43fb22fe936bd93311
o-ga <54522966+o-ga09@users.noreply.github.com>
2023-04-17 14:57:39 +0900

    refact: #4 tailwindcssへ修正

--- src/pages/self-introduction.vue
+++ src/pages/self-introduction.vue
@@ -1,14 +1,13 @@
 <template>
     <div id="app">
-        <div class="main_image">
-            <img src="../assets/images/main-image.jpg" width="100" alt="main_image" >
-        </div>
-        <h2>I am @o-ga09！！</h2>
-        <p>社会人３年目のGolang大好きエンジニアです。</p>
+        <img class="w-12 h-52 block m-auto mt-12 rounded-full" src="../assets/images/main-image.jpg" width="100" alt="main_image" >
+       
+        <h2 class="flex justify-center pt-5 pb-5 text-4xl">I am @o-ga09！！</h2>
+        <p class="flex justify-center pt-2 text-2xl">社会人３年目のGolang大好きエンジニアです。</p>
         <br>
-        <p>I like Golang/Vue/GCP/k8s</p>
+        <p class="flex justify-center pt-2 text-2xl">I like Golang/Vue/GCP/k8s</p>
         <br>
-        <p>I have FE(基本情報技術者)</p>
+        <p class="flex justify-center pt-2 text-2xl">I have FE(基本情報技術者)</p>
         <br>
     </div>
 </template>
@@ -18,48 +17,3 @@ export default {
 
 }
 </script>
-
-<style>
-    .main_image img{
-        width: 200px;
-        height: 200px;
-        display: block;
-        margin: auto;
-        margin-top: 50px;
-        border-radius: 50%;
-    }
-
-    h2 {
-        text-align: center;
-        padding-top: 20px;
-        padding-bottom: 20px;
-        font-size: 40px;
-        font-weight: bold;
-    }
-    p {
-        text-align: center;
-        padding-top: 10px;
-        font-size: 20px;
-    }
-
-    .wrap {
-        margin: auto;
-        margin-top: 50px;
-        width: 270px;
-        display: flex;
-    }
-
-    .icon {
-        width:100px;
-        height:100px; 
-    }
-
-    .icon-twitter {
-        fill: aqua;
-    }
-
-    .icon-github {
-        margin-left: 70px;
-        fill: lime;
-    }
-</style>
