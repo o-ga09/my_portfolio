408b907a969dafc7f88a5e78c463d14bb01ea98f
o-ga <54522966+o-ga09@users.noreply.github.com>
2023-04-17 14:46:11 +0900

    refact: #6  ヘッダーのデザインを変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,12 @@
 <template>
-    <header class="text-gray-600 body-font">
-      <div class="header-container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a href="/" class="mr-5 hover:text-gray-900">Home</a>
-          <a href="https://sites.google.com/view/profile202303/%E3%83%9B%E3%83%BC%E3%83%A0" class="mr-5 hover:text-gray-900">Profile</a>
-          <a href="https://sites.google.com/view/skills-202303/%E3%83%9B%E3%83%BC%E3%83%A0" class="mr-5 hover:text-gray-900">Skills</a>
-          <a href="https://sites.google.com/view/vlogs202303/%E3%83%9B%E3%83%BC%E3%83%A0" class="mr-5 hover:text-gray-900">Blog</a>
+    <header class="h-2/5 mb-10 bg-amber-200">
+        <h1 class="flex font-bold text-2xl md:text-4xl justify-center">test</h1>
+        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center md:justify-end">
+          <a href="/" class="mr-5 hover:text-red-500">Home</a>
+          <a href="" class="mr-5 hover:text-red-500">Profile</a>
+          <a href="" class="mr-5 hover:text-red-500">Skills</a>
+          <a href="" class="mr-5 hover:text-red-500">Blog</a>
         </nav>
-      </div>
     </header>
 </template>
 
